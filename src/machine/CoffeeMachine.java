775958af04811879d0839b56374baa632cb7dba0
HerinOd <hieudo851@gmail.com>
2026-01-29 01:14:51 +0700

    2, Calculate the ingredients

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -1,7 +1,10 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        //==========================================================
         System.out.println("Starting to make a coffee\n" +
                 "Grinding coffee beans\n" +
                 "Boiling water\n" +
@@ -9,5 +12,22 @@ public class CoffeeMachine {
                 "Pouring coffee into the cup\n" +
                 "Pouring some milk into the cup\n" +
                 "Coffee is ready!");
+        //==========================================================
+        Scanner scanner = new Scanner(System.in);
+
+        //Ask the user to input how many cups of coffee they need:
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsOfCoffee = scanner.nextInt();
+
+        //The ingredients needed for each cup:
+        int waterEachCup = 200; //ml
+        int milkEachCup = 50; //ml
+        int coffeeBeansEachCup = 15; //g
+
+        //Output the required ingredient amounts back to the user:
+        System.out.println("For " + cupsOfCoffee + " cups of coffee you will need:\n" +
+                (waterEachCup * cupsOfCoffee) + " ml of water\n" +
+                (milkEachCup * cupsOfCoffee) + " ml of milk\n" +
+                (coffeeBeansEachCup * cupsOfCoffee) + " g of coffee beans");
     }
 }
